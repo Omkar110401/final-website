1d662d1bddfb7bdb93a6d21011d293a0dbae3822
Omkar110401 <78097888+Omkar110401@users.noreply.github.com>
2023-09-14 09:29:51 +0530

    changes in footer

--- src/app/skills-maxim/skills-maxim.component.ts
+++ src/app/skills-maxim/skills-maxim.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills-maxim',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills-maxim.component.css']
 })
 export class SkillsMaximComponent {
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
 
+  ngOnInit() {
+    // Change the background color of the root HTML element (e.g., <html>)
+    this.renderer.setStyle(document.documentElement, 'background-color', 'whitesmoke');
+
+    // Alternatively, you can change the background color of the body element:
+    // this.renderer.setStyle(document.body, 'background-color', 'red');
+  }
 }
