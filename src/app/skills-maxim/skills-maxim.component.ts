9e773be673c744a6bb6bb01d40e7cb8585d04457
Omkar110401 <78097888+Omkar110401@users.noreply.github.com>
2023-09-14 14:47:28 +0530

    added projects and redirects for not found

--- src/app/skills-maxim/skills-maxim.component.ts
+++ src/app/skills-maxim/skills-maxim.component.ts
@@ -1,4 +1,5 @@
 import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-skills-maxim',
@@ -6,12 +7,11 @@ import { Component, Renderer2, ElementRef, OnInit } from '@angular/core';
   styleUrls: ['./skills-maxim.component.css']
 })
 export class SkillsMaximComponent {
-  constructor(private renderer: Renderer2, private el: ElementRef) { }
+  constructor(private renderer: Renderer2, private el: ElementRef, private router:Router) { }
 
   ngOnInit() {
     // Change the background color of the root HTML element (e.g., <html>)
     this.renderer.setStyle(document.documentElement, 'background-color', 'whitesmoke');
-
     // Alternatively, you can change the background color of the body element:
     // this.renderer.setStyle(document.body, 'background-color', 'red');
   }
