349ed620fcb2fb8ccd60ab8f817fabe0f0034bab
Omkar110401 <78097888+Omkar110401@users.noreply.github.com>
2023-09-18 09:36:15 +0530

    changed not found page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, AfterViewInit, Renderer2} from '@angular/core';
+import * as Parallax from 'parallax-js';
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements AfterViewInit{
+  private scene!: HTMLElement;
+  private parallax!: Parallax;
 
+  constructor(private elementRef: ElementRef, private renderer:Renderer2) {}
+  ngOnInit(){
+    
+  }
+  ngAfterViewInit() {
+    this.renderer.setStyle(document.body, 'background-color', 'purple');
+    // Find the scene element within the component's view
+    this.scene = this.elementRef.nativeElement.querySelector('#scene');
+
+    // Initialize Parallax after the view has been initialized
+    if (this.scene) {
+      this.parallax = new Parallax(this.scene);
+    }
+  }
 }
