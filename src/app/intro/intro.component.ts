fb7287885724e3094c18a8f38405b936c0bec728
Omkar110401 <78097888+Omkar110401@users.noreply.github.com>
2023-09-22 18:15:10 +0530

    added dynamic text in intro

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-intro',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.css']
 })
 export class IntroComponent {
+  @ViewChild('text', { static: true }) textElement!: ElementRef;
+  @ViewChild('cursor', { static: true }) cursorElement!: ElementRef;
+
+  private textContent = "Data Analysis, Machine Learning & Deep Learning.";
+  private currentIndex = 0;
+  private typingComplete = false;
+  constructor() { }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.typeText();
+    }, 2000); // Add a 2-second delay (adjust as needed)
+  }
+
+  typeText() {
+    if (this.currentIndex <= this.textContent.length) {
+      const currentText = this.textContent.slice(0, this.currentIndex);
+      this.textElement.nativeElement.textContent = currentText;
+      this.currentIndex++;
   
+      // Update the cursor's position
+      this.cursorElement.nativeElement.style.left = `${currentText.length * 12}px`; // Adjust the spacing
+  
+      setTimeout(() => {
+        this.typeText();
+      }, 500); // Adjust typing speed (milliseconds)
+    } else {
+      this.cursorElement.nativeElement.style.display = 'none';
+      this.typingComplete = true; // Set the typing status to complete
+    }
+  }
 }
